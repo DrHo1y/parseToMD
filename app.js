5cee3d19bedc664c973094c8ba0d84d6bbfa7212
DrHo1y <dr.holyblack@gmail.com>
2025-03-26 14:09:50 +0700

    add parse urls on habr.com

--- app.js
+++ app.js
@@ -2,49 +2,70 @@ const TurndownService = require('turndown')
 const { JSDOM } = require('jsdom')
 const rp = require('request-promise')
 const fs = require('node:fs')
-
-async function parseHTML(urls, _class) {
+async function parseHTML(urls, tagName) {
     try {
         const len = urls.length
         for (let i = 0; i < len; i++) {
+
             const html = await rp(urls[i])
 
             const dom = new JSDOM(html)
             const document = dom.window.document
-            const name = document.getElementsByClassName('tm-title tm-title_h1').item(0).textContent.replace(new RegExp('[|*?<>:\\\n\r\t\v]', 'gm'), '')
-            const res = document.getElementsByTagName(_class).item(0).outerHTML
+            const name = document.getElementsByClassName('tm-title tm-title_h1')
+                .item(0).textContent.replace(new RegExp('[|*?<>:\\\n\r\t\v]', 'gm'), '')
+            const res = document.getElementsByTagName(tagName).item(0).outerHTML
 
             const turndownService = new TurndownService()
             const markdown = turndownService.turndown(res)
-            const filename = `content/${i+1}-${name}.md`
+            const filename = `content1/${i+1}-${name}.md`
             fs.writeFile(filename, markdown, err => {
                 if (err) {
                     console.error(err)
                 } else {
-                    console.log(`File save as ${filename}`)
+                    // console.log(`File save as ${filename}`)
                 }
             })
+            console.log((i+1) + '/' + len)
         }
-
     } catch (e) {
         console.error(e)
     }
 }
 
-// Example Articles:
+async function pasreArticlesURL(url, query) {
+    try {
+        const html = await rp(url)
+        const dom = new JSDOM(html)
 
-const urls = [
-    'https://habr.com/ru/companies/selectel/articles/893268/',
-    'https://habr.com/ru/articles/892860/',
-    'https://habr.com/ru/companies/ru_mts/articles/888756/',
-    'https://habr.com/ru/companies/timeweb/articles/885626/',
-    'https://habr.com/ru/articles/887956/',
-    'https://habr.com/ru/companies/ru_mts/articles/887368/',
-    'https://habr.com/ru/companies/ru_mts/articles/887068/',
-    'https://habr.com/ru/companies/otus/articles/884434/',
-    'https://habr.com/ru/companies/tractorpilot/articles/880596/',
-    'https://habr.com/ru/companies/ascon/articles/874490/'
+        const document = dom.window.document
+        var urls = document.querySelectorAll(query)
+        var resURLS = []
+        var urlHttps = 'https://' + url.split('/')[2]
+        urls.forEach(url => {
+            let res = urlHttps + url.href
+            resURLS.push(res)
+        })
+        return resURLS
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-]
+async function main() {
+    const query = 'div.tm-article-snippet.tm-article-snippet > h2 > a'
+    var masURL = []
 
-parseHTML(urls, 'article')
+    for (let i = 1; i < 11; i++)
+    {
+        let urlsT = await pasreArticlesURL(`https://habr.com/ru/hubs/DIY/articles/page${i}/`, query)
+        masURL.push(urlsT)
+    }
+    masURL = masURL.flat()
+    var time = Date.now()
+    await parseHTML(masURL, 'article').then(() => {
+        time = (Date.now() - time) / 1000
+        console.log(`Complite! ${time}s`)
+    })
+}
+
+main()
